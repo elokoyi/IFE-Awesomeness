c78b1eb06228db180cd1941fc31293547bf6ae6f
EC2 Default User <ec2-user@ip-172-31-19-114.us-east-2.compute.internal>
2019-08-04 21:32:38 +0000

    some sort of comment

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,17 @@
 import os
 from app import app
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for
 
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
+app.secret_key = b'\xb1\xe2\x9fs1I\x80\x7f\xd1\xd6 \xf3\xear\xfb\x06'
 
-
-# from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo
 
 # name of database
-# app.config['MONGO_DBNAME'] = 'database-name' 
+app.config['MONGO_DBNAME'] = 'connect' 
 
 # URI of database
-# app.config['MONGO_URI'] = 'mongo-uri' 
-
-# mongo = PyMongo(app)
+app.config['MONGO_URI'] = 'mongodb+srv://connectuser:TTOoMBxlK16XpKtJ@cluster0-gbcn0.mongodb.net/connect?retryWrites=true&w=majority' 
+mongo = PyMongo(app)
 
 
 # INDEX
@@ -28,15 +22,75 @@ events = [
 def index():
     return render_template('index.html')
 
+@app.route('/no-sidebar.html', methods = ["GET", "POST"])
+def nosidebar():
+    return render_template("no-sidebar.html")
+    
 
-# CONNECT TO DB, ADD DATA
+@app.route('/right-sidebar.html', methods = ["GET", "POST"])
+def rightsidebar():
+    return render_template("right-sidebar.html")
 
-#@app.route('/add')
+@app.route('/left-sidebar.html', methods = ["GET", "POST"])
+def lefttsidebar():
+    return render_template("left-sidebar.html")
+    
 
-#def add():
-    # connect to the database
+@app.route('/mission.html', methods = ["GET", "POST"])
+def mission():
+    return render_template("mission.html")
+    
 
-    # insert new data
+@app.route('/signup.html', methods = ["GET", "POST"])
+def signuppage():
+    return render_template("signup.html")
 
-    # return a message to the user
-   # return ""
+@app.route('/home.html', methods = ["GET", "POST"])
+def home():
+    return render_template("home.html")
+    
+# @app.route('/info.html', methods = ["GET", "POST"])
+# def info():
+#     return render_template("")
+
+
+# SIGN-UP:
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    if request.method =="POST":
+        # take in the info they gave us, check if username is taken, if username is available put into a database of user
+        users = mongo.db.users
+        existing_user = users.find_one({"username":request.form['username']})
+        if existing_user is None:
+            users.insert({"username":request.form['username'],"password":request.form['password'],"email":request.form['email'],"name":request.form['name']})
+            return render_template("info.html")
+        else:
+            return render_template("/useduser.html")
+    else:
+        return redirect("/index")
+        
+
+#Log In:
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    # use the username to find the account
+    existing_user = users.find_one({"username":request.form['username']})
+    if existing_user:
+        # check if the password is right
+        if existing_user['password'] == request.form['password'] :
+            # session['username'] = request.form['username']
+            return redirect(url_for('home'))
+        else:
+            return "Your password doesn't match your username."
+    else:
+        return "There is no user with that username. Try making an account."
+  
+#   LOG OUT
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
+
+
